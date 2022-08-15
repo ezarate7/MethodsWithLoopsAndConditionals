b7f5f3e6525a742591da9ea98f6836c787e431f6
Eddie Z <e.zarate7@hotmail.com>
2022-08-15 10:17:47 -0600

    initial commit

--- MethodsWithLoopsAndConditionals/Program.cs
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -1,29 +1,21 @@
-﻿//Write a method that will print to the console all numbers 1000 through -1000.
-for (int i = 1000; i >= -1000; i--)
-{
-    Console.WriteLine(i);
-}
-//Write a method that will print to the console numbers 3 through 999 by 3 each time.
+﻿
+using MethodsWithLoopsAndConditionals;
+Methods.Numbers1000toNeg1000();
+
+Methods.Threes();
+
+
+Methods.CheckIfEqual(1, 2);
+
+Methods.Equals(121, 123);
+
+Methods.IsPositiveOrNegative(122);
+
+Methods.VotingAge();
 
 
 
-for (int k = 3; k <= 999; k += 3)
-{
-    Console.WriteLine(k);
-}
-
-
-//Write a method to accept two integers as parameterss and check whether they are equal or not
-
-    static bool IsEqual(int a, int b)
-  { 
-    if (a == b)
-    {
-        return true;
-    }
-
-    return false;
 
 
 
-  }
+
